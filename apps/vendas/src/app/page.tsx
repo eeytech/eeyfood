816554ab4189a99e09ea4d82fb835328476854dc
Matheus Willian Bento <eeytecnologia@gmail.com>
2026-06-11 15:09:50 -0300

    1

--- apps/vendas/src/app/page.tsx
+++ apps/vendas/src/app/page.tsx
@@ -4,7 +4,7 @@ import { GlobeIcon, MessageCircleIcon, ShoppingBagIcon, UtensilsIcon } from "luc
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
@@ -18,7 +18,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-const HomePage = () => {
+const HomeContent = () => {
   const [restaurantName, setRestaurantName] = useState("");
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -143,6 +143,14 @@ const HomePage = () => {
         </div>
       </div>
     </main>
+  );
+};
+
+const HomePage = () => {
+  return (
+    <Suspense>
+      <HomeContent />
+    </Suspense>
   );
 };
 
