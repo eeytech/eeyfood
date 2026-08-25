eea0b4f7261db57917f2fc06c441da7bc9c66663
Matheus Willian Bento <eeytecnologia@gmail.com>
2026-08-25 19:15:33 -0300

    a1

--- apps/gestao/next.config.ts
+++ apps/gestao/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
   },
-  serverExternalPackages: ["sharp", "mercadopago", "pg", "openai"],
+  serverExternalPackages: ["sharp", "pg"],
   outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
